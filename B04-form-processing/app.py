d9e57b273a1e14b2e2e4ff14e174ba23f07b8044
4forces <toh.tingji@gmail.com>
2020-09-22 11:31:08 +0000

    b04 up forms-return.template done

--- B04-form-processing/app.py
+++ B04-form-processing/app.py
@@ -7,10 +7,53 @@ app = Flask(__name__)
 def home():
     return render_template('home.template.html')
 
+@app.route('/num-game')
+def number_game():
+    return render_template('input.template.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.template.html')
+@app.route('/num-game', methods=['POST'])
+def process_number():
+    num = int(request.form.get('number'))
+    return render_template('process_num.template.html', num=num)
+
+
+# @app.route('/about')
+# def about():
+#     return render_template('about.template.html')
+
+@app.route('/contact-us', methods=['GET'])
+def contact():
+    return render_template('form.template.html')
+
+
+@app.route('/contact-us', methods=['POST'])
+def process_contact():
+
+    print(request.form)
+
+    name = request.form.get('name')
+    sex = request.form.get('sex')
+    comment = request.form.get('comment')
+    can_contact = request.form.get('can-contact')
+    termscons = request.form.get('democheckbox')
+
+    contactable = ""
+
+    if can_contact == "1":
+        contactable = True
+    else:
+        contactable = False
+
+    if termscons == None:
+        return render_template("form.template.html",
+                               message="Please accept the checkbox")
+
+    return render_template('form-return.template.html', 
+                           name=name, 
+                           sex=sex,
+                           comment=comment,
+                           contactable=contactable,
+                           termscons=termscons)
 
 
 # "magic code" -- boilerplate
