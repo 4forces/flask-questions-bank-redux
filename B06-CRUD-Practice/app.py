b69188c360849c02a8c04c41fab73ce508192102
4forces <toh.tingji@gmail.com>
2020-09-22 13:16:24 +0000

    B06 form creation partially done

---
+++ B06-CRUD-Practice/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+import json
+import os
+import random
+
+app = Flask(__name__)
+
+database = []
+with open('data.json', 'r') as fp:
+    database = json.load(fp)
+
+@app.route("/")
+def home():
+    return "Home page"
+
+@app.route("/ufo-sightings")
+def show_sightings():
+    return render_template('sightings.template.html', database=database)
+
+@app.route('/sightings/create')
+def create_sightings():
+    return render_template('form.template.html')
+
+@app.route('/sightings/create', methods=["POST"])
+def process_create_sightings():
+    new_sighting = {
+        "id": random.randint(10000,99999),
+        "title": request.form.get("title"),
+        "author": request.form.get("author")
+        }
+    database.append(new_sighting)
+
+    with open('data.json', 'w') as fp:
+        json.dump(database, fp)
+
+    # return "form received"
+
+    return redirect(url_for('show_sightings'))
+
+
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
